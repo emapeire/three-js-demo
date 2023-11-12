28a2ea9e880486f6a87a5e8a5fec0fa1eca2a962
Emanuel Peire <emapeire@outlook.com>
2023-11-11 21:48:39 -0300

    add pyramid

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import * as THREE from "three";
+import * as CANNON from "cannon";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  100,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -12,19 +13,25 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const geometry = new THREE.ConeGeometry(2, 2, 4);
+const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
+const floorGeometry = new THREE.PlaneGeometry(100, 100, 100);
+const floorMaterial = new THREE.MeshBasicMaterial({ color: 0x999999 });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = -Math.PI / 2;
+floor.position.y = -1;
+scene.add(floor);
+
+renderer.setClearColor(0x888888, 1);
+
 function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 
